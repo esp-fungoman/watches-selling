5be282729750cde3e0aa7cfdb657f2f011293608
Cerberose <tranlethaison123@gmail.com>
2022-10-09 12:34:09 +0700

    update

---
+++ components/ProductPanel/ProductPanel.tsx
@@ -0,0 +1,55 @@
+import styles from "./ProductPanel.module.scss";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import { Rating } from "@mui/material";
+
+interface IProductPanel {
+  product: { [key: string]: any}
+}
+
+
+const ProductPanel = (props: IProductPanel) => {
+  const { product } = props;
+  const router = useRouter();
+
+  const title = product.title.length <= 27 ? 
+  product.title : product.title.slice(0, 27) + "...";
+
+  return (
+    <div className={styles.panel} onClick={() => router.push(product.link)}>
+      <div className={styles.image_wrapper}>
+        <Image 
+          className={styles.image}
+          src={product.image} 
+          width={122} 
+          height={102} 
+          layout="responsive" 
+          alt=""
+        />
+        <div className={styles.top_tag}>{product.top_tag}</div>
+      </div>
+      
+      <div className={styles.sub_title}>
+        <div className={styles.brand_wrapper}> 
+          <div className={styles.line}></div>
+          <div className={styles.brand}>{product.brand}</div>
+        </div>
+        <div className={styles.sale_tag}>{product.sale_tag}</div>
+      </div>
+
+      <div className={styles.title}>{title}</div>
+
+      <div className={styles.rating}>
+        <Rating size="small" value={product.rating.star} readOnly />
+        <div className={styles.number}>({product.rating.number})</div>
+      </div>
+
+      <div className={styles.price}>
+        <div className={styles.show_price}>{product.saled_price ? product.saled_price : product.price}</div>
+        <div className={styles.origin_price}>{product.saled_price ? product.price : ""}</div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductPanel;
---
+++ pages/index.tsx
@@ -0,0 +1,200 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header/Header'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer/Footer'
+import Carousel from '../components/CarouselBanner/Carousel'
+import Image from 'next/image';
+import { useRouter } from 'next/router'
+import ProductPanel from '../components/ProductPanel/ProductPanel'
+
+const bannerItem = [
+  {
+    link: "#",
+    image: "/assets/homepage/banner/banner.png"
+  },
+  {
+    link: "#",
+    image: "/assets/homepage/banner/banner.png"
+  },
+  {
+    link: "#",
+    image: "/assets/homepage/banner/banner.png"
+  },
+  {
+    link: "#",
+    image: "/assets/homepage/banner/banner.png"
+  },
+  {
+    link: "#",
+    image: "/assets/homepage/banner/banner.png"
+  }
+]
+
+const listItem = [
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: "12"
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  },
+  {
+    image: "/assets/homepage/shopping-item/item-thumbnail.png",
+    link: "#",
+    brand: "LANEIGE",
+    sale_tag: "-40%",
+    title: "Kem dưỡng da chiết xuất hoa lan tuyết Jeju Hàn Quốc",
+    rating: {
+      star: 5,
+      number: 12
+    },
+    saled_price: "200 000đ",
+    price: "950 000đ",
+    top_tag: "Hot deal",
+    sold_out: false
+  }
+]
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  
+
+  return (
+    <div>
+      <Head>
+        <title>CheriCT</title>
+      </Head>
+      
+      <div>
+        <Header/>
+        <div>
+          <Carousel>
+            {bannerItem.map((item: any, index: any) => (
+              <div key={index} onClick={() => router.push(item.link || "")}>
+                <Image 
+                  src={item.image || require("public/vercel.svg")} 
+                  layout="responsive" 
+                  width={1440} 
+                  height={482} 
+                  alt=""
+                  />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+        <div>
+          <div className="container">
+            <Carousel productCarousel={true}>
+              {listItem.map((item: any, index: any) => (
+                <ProductPanel key={index} product={item} />
+              ))}
+            </Carousel>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
