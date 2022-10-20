094a63be198167bc63832354b477f08674537e28
Cerberose <tranlethaison123@gmail.com>
2022-10-20 18:13:42 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <title>
-      </title>
+      <title>CheriCT</title>
       <Head>
         <meta
           name="google-site-verification"
