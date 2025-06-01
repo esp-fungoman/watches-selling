3bf9b4db9fa38abea7b09a71f76afeb74cd7261d
DamTuanPhat <damtuanphat@gmail.com>
2025-06-01 15:05:06 +0700

    update: integrate order detail

--- pages/invoice/[slug].tsx
+++ pages/invoice/[slug].tsx
@@ -1,20 +1,14 @@
 import { notification, Switch, Table } from "antd";
 import Divider from "../../components/Divider";
-import { useEffect, useState } from "react";
-import { add, format } from "date-fns";
+import { useEffect, useMemo, useState } from "react";
+import { add, format, parseISO } from "date-fns";
 import styles from "./Invoice.module.scss";
 import { ColumnsType } from "antd/es/table";
 import { useRouter } from "next/router";
 import { InvoiceApi } from "../../services/invoice";
 import { formatPrice } from "../../helpers/data.helpers";
 const InvoiceDetail = () => {
-  const [data, setData] = useState<any>({
-    receiverName: "Nam",
-    receiverAddress: "Hue",
-    receiverPhoneNumber: "0209291991",
-    orderDate: new Date(),
-    estimateDeliveryDate: add(new Date(), { days: 3 }),
-  });
+  const [data, setData] = useState<any>();
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
 
@@ -22,12 +16,24 @@ const InvoiceDetail = () => {
     setLoading(true);
     const id = router?.asPath?.split("/")[2] as string;
     if (id && id !== "[slug]") {
-      InvoiceApi.detail(id).then((res) => setData(res));
+      InvoiceApi.detail(id).then((res) => {
+        console.log("🚀 ~ InvoiceApi.detail ~ res:", res);
+        setData(res);
+      });
     }
     setLoading(false);
   }, [router]);
 
-  const dataSource = data?.orderDetails?.map((item: any, index: number) => ({
+  const totalMoney = useMemo(() => {
+    if (!data?.details) return 0;
+    let total = data.details.reduce(
+      (sum: number, item: any) => sum + item.price * item.quantity,
+      0
+    );
+    return total + total * 0.08;
+  }, [data?.details]);
+
+  const dataSource = data?.details?.map((item: any, index: number) => ({
     ...item,
     index: index + 1, // Auto-incrementing index starting from 1
   }));
@@ -44,14 +50,14 @@ const InvoiceDetail = () => {
       width: 400,
       key: "name",
       align: "left",
-      render: (_, record) => <div>{record.watch.name}</div>,
+      render: (_, record) => <div>{record.name}</div>,
     },
     {
       title: "Giá",
       width: 200,
       key: "price",
       align: "center",
-      render: (_, record) => <div>{formatPrice(record.watch.price)}</div>,
+      render: (_, record) => <div>{formatPrice(record.price)}</div>,
     },
     {
       title: "Số lượng",
@@ -66,7 +72,7 @@ const InvoiceDetail = () => {
       key: "total",
       align: "center",
       render: (_, record) => (
-        <div>{formatPrice(record.quantity * record.watch.price)}</div>
+        <div>{formatPrice(record.quantity * record.price)}</div>
       ),
     },
   ];
@@ -81,29 +87,31 @@ const InvoiceDetail = () => {
             <div className="flex justify-start mb-2">
               <div className="flex gap-3 flex-1">
                 <div className="font-bold ">Receiver Name:</div>
-                <div>{data.receiverName}</div>
+                <div>{data?.delivery_information?.full_name}</div>
               </div>
               <div className="flex gap-3 w-[250px] justify-start">
                 <div className="font-bold ">Phone Number:</div>
-                <div>{data.receiverPhoneNumber}</div>
+                <div>{data?.delivery_information?.phone_number}</div>
               </div>
             </div>
             <div className="flex justify-start mb-2">
-              <div className="flex gap-3 flex-1">
+              {/* <div className="flex gap-3 flex-1">
                 <div className="font-bold ">Bill To:</div>
-                <div>{data.receiverAddress}</div>
-              </div>
-              <div className="flex gap-3 justify-start w-[250px]">
-                <div className="font-bold ">Order Date:</div>
-                <div>{format(data.orderDate, "dd/MM/yyyy")}</div>
-              </div>
+                <div>{data.delivery_information}</div>
+              </div> */}
+              {data?.created_at && (
+                <div className="flex gap-3 justify-start w-[250px]">
+                  <div className="font-bold ">Order Date:</div>
+                  <div>{format(parseISO(data?.created_at), "dd/MM/yyyy")}</div>
+                </div>
+              )}
             </div>
-            <div className="flex justify-start  ">
+            {/* <div className="flex justify-start  ">
               <div className="flex gap-3 w-max">
                 <div className="font-bold ">Delivery Date (estimate)</div>
                 <div>{format(data.estimateDeliveryDate, "dd/MM/yyyy")}</div>
               </div>
-            </div>
+            </div> */}
           </div>
           <Table
             columns={columns}
@@ -114,9 +122,11 @@ const InvoiceDetail = () => {
             footer={() => (
               <div className="flex items-center w-full">
                 <div className="flex-1"></div>
-                <div className="w-[150px] text-center font-bold">Tổng tiền (8% Thuế)</div>
+                <div className="w-[150px] text-center font-bold">
+                  Tổng tiền (8% Thuế)
+                </div>
                 <div className="w-[120px] text-center font-bold">
-                  {formatPrice(data.totalPriceAfterTax)} 
+                  {formatPrice(totalMoney)}
                 </div>
               </div>
             )}
